b5ea83596e5b51e4400b5ecc6f717f195bb1c4d8
skilleen95 <80342759+skilleen95@users.noreply.github.com>
2021-07-16 19:11:53 +0000

    Create TaxCalculatorTest.java

---
+++ src/test/java/com/skilleen/secondmicroservice/TaxCalculatorTest.java
@@ -0,0 +1,27 @@
+package com.skilleen.secondmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaxCalculatorTest {
+
+    TaxCalculator taxCalculator = new TaxCalculator();
+
+    @Test
+    void givenOrderPriceAndTax_addTaxToOrder_shouldSumThePriceAndTax() {
+        // INPUT
+        double price = 10;
+        double tax = 3;
+
+        // Execution
+        double result = taxCalculator.addTaxToOrder(price, tax);
+
+        // OUTPUT
+        assertThat(result).isEqualTo(13);
+
+    }
+}
