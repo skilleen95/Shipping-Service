52b8e3cce3b9597e0fe1911db2520d7d7802289a
sivapeddareddigari <sivapeddareddigari@gmail.com>
2021-07-20 17:15:20 -0400

    Update TaxCalculator.java

--- src/main/java/com/skilleen/secondmicroservice/TaxCalculator.java
+++ src/main/java/com/skilleen/secondmicroservice/TaxCalculator.java
@@ -3,7 +3,7 @@ package com.skilleen.secondmicroservice;
 public class TaxCalculator {
 
     public double addTaxToOrder(double orderPrice, double tax) {
-        return orderPrice + tax;
+        return orderPrice - tax;
     }
 
 }
